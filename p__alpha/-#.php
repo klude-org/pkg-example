10a4fb25374abc5ad0e58145624a70a1b07da2a4
klude-au <124860775+klude-au@users.noreply.github.com>
2025-02-02 21:05:57 +1030

    update: added plugins

---
+++ p__alpha/-#.php
@@ -0,0 +1,7 @@
+<?php namespace pkg_github__klude_org__example;
+
+class p__alpha {
+    public function prt(){ echo static::class.": main\n"; }
+}
+
+
---
+++ p__charlie/-#.php
@@ -0,0 +1,6 @@
+<?php namespace pkg_github__klude_org__example;
+
+class p__bravo {
+    public function prt(){ echo static::class.": main\n"; }
+}
+
