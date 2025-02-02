9d54fe8f939e91ca03e067af72708fb4526ccf25
klude-au <124860775+klude-au@users.noreply.github.com>
2025-02-02 22:47:23 +1030

    update

--- p__alpha/-#.php
+++ p__alpha/-#.php
@@ -1,5 +1,5 @@
 <?php namespace p__alpha;
 
 class github__klude_org__example {
-    public function prt(){ echo static::class.": branch/250202-01-dev\n"; }
+    public function prt(){ echo static::class.": main\n"; }
 }
--- p__bravo/-#.php
+++ p__bravo/-#.php
@@ -1,6 +1,6 @@
 <?php namespace p__bravo;
 
 class github__klude_org__example {
-    public function prt(){ echo static::class.": branch/250202-01-dev\n"; }
+    public function prt(){ echo static::class.": main\n"; }
 }
 
